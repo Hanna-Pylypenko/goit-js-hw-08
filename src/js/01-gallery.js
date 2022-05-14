396a380640a38dd099c6d6099131216aa56f9b14
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-05-14 22:41:33 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,5 @@
 import { galleryItems } from './gallery-items.js';
-
 import SimpleLightbox from 'simplelightbox';
-
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
@@ -13,8 +11,8 @@ galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 function createGalleryItemsMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item"> <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" /></a> </div>`;
+      return `<li class="gallery__item"> <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /></a> </li>`;
     })
     .join(' ');
 }

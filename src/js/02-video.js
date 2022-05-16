e4a7a5eef4a25c2f262a9b2c40fac8c3f9a22296
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-05-16 09:35:50 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 //player.addEventListener('timeupdate', throttle(1000));
-player.on('timeupdate', function ({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-  return seconds;
-});
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+    return seconds;
+  }, 1000),
+);
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
 player.getVideoTitle().then(function (title) {

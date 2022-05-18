b872fe1ea49a89b3c528e2aee95aa5a276782b00
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-05-18 11:15:20 +0300

    video update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,16 +11,16 @@ player.on(
   }, 1000),
 );
 
-// if (localStorage.getItem('videoplayer-current-time')) {
-//   currentTime = localStorage.getItem('videoplayer-current-time');
-//   player.setCurrentTime(currentTime);
-// }
-if (!localStorage.getItem('videoplayer-current-time')) {
-  let currentTime = 0;
-} else {
+if (localStorage.getItem('videoplayer-current-time')) {
   currentTime = localStorage.getItem('videoplayer-current-time');
   player.setCurrentTime(currentTime);
 }
+// if (!localStorage.getItem('videoplayer-current-time')) {
+//   let currentTime = 0;
+// } else {
+//   currentTime = localStorage.getItem('videoplayer-current-time');
+//   player.setCurrentTime(currentTime);
+// }
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
